f79d91e0ad4087499d2e0c20b27620de32b9c5ab
Rothiii <drome.emord@gmail.com>
2024-03-07 11:55:36 +0800

    Fixing params  update, delete

--- src/features/account/accountRoute.ts
+++ src/features/account/accountRoute.ts
@@ -6,9 +6,12 @@ const accountRoute = Router();
 // Get all employees
 accountRoute.get('/employees/:company_branch_id', AccountController.getAllEmployees);
 // Create a new employee
-accountRoute.post('/employees', AccountController.createEmployee);
-// Update an employee
-accountRoute.patch('/employees/:id', AccountController.updateEmployee);
+accountRoute.post('/employees/', AccountController.createEmployee);
+// Update an employee use params
+// accountRoute.patch('/employees/:company_branch_id/:employee_id', AccountController.updateEmployee);
+
+// Update an employee use body
+accountRoute.patch('/employees/', AccountController.updateEmployee);
 // Delete an employee
-accountRoute.delete('/employees/:id', AccountController.deleteEmployee);
+accountRoute.delete('/employees/:company_branch_id/:employee_id', AccountController.deleteEmployee);
 export default accountRoute;
