5388a31222325e035831fef8fb371950d54e7751
Rothiii <drome.emord@gmail.com>
2024-03-08 03:30:32 +0800

    Done
    bismillah done

--- src/features/account/accountService.ts
+++ src/features/account/accountService.ts
@@ -1,5 +1,6 @@
 import { Validation } from '../../validations';
 import { AccountValidation } from './accountValidation';
+import { hashPassword } from '../../utils';
 import {
   CreateRequest,
   CreateResponse,
@@ -19,12 +20,16 @@ export class AccountService {
   }
 
   static async createEmployee(
-    employeeData: CreateRequest,
+    employeeData: CreateRequest
   ): Promise<CreateResponse> {
+    employeeData.identity_expired_date = new Date(employeeData.identity_expired_date);
+    employeeData.birth_date = new Date(employeeData.birth_date);
+    
     const request = Validation.validate(
       AccountValidation.CREATE_EMPLOYEE,
       employeeData
     );
+
 
     const countEmployee = await prisma.employee.count({
       where: { email: request.email },
@@ -39,40 +44,73 @@ export class AccountService {
       );
     }
 
-    // const newEmployee = await prisma.employee.create({
-    //   data: {
-    //     employee_id: request.employee_id,
-    //     company_branch_id: request.company_branch_id,
-    //     job_position_id: request.job_position_id,
-    //     employment_status_id: request.employment_status_id,
-    //     unique_id: request.unique_id,
-    //     first_name: request.first_name,
-    //     last_name: request.last_name,
-    //     email: request.email,
-    //     password: request.password,
-    //     phone_number: request.phone_number,
-    //     place_of_birth: request.place_of_birth,
-    //     birth_date: request.date_of_birth,
-    //     marital_status: request.marital_status,
-    //     blood_type: request.blood_type,
-    //     religion: request.religion,
-    //     identity_type: request.identity_type,
-    //     identity_number: request.identity_number,
-    //     identity_expired_date: request.identity_expired_date,
-    //     postcal_code: request.postcal_code,
-    //     citizen_id_address: request.citizen_id_address,
-    //     residential_address: request.residential_address,
-    //     bank_account_number: request.bank_account_number,
-    //     bank_type: request.bank_type,
-    //     wage: request.wage,
-    //   },
-    // });
+    const existingJobPosition = await prisma.jobPosition.findFirst({
+      where: {
+        company_branch_id: request.company_branch_id,
+        job_position_id: request.job_position_id,
+      },
+    });
+
+    if (!existingJobPosition) {
+      await prisma.jobPosition.create({
+        data: {
+          company_branch_id: request.company_branch_id,
+          name: request.job_position_name,
+        },
+      });
+    }
+
+    const existingEmploymentStatus = await prisma.employmentStatus.findFirst({
+      where: {
+        company_branch_id: request.company_branch_id,
+        employment_status_id: request.employment_status_id,
+      },
+    });
+
+    if (!existingEmploymentStatus) {
+      await prisma.employmentStatus.create({
+        data: {
+          company_branch_id: request.company_branch_id,
+          name: request.employment_status_name,
+        },
+      });
+    }
+
+    const hashedPassword = hashPassword(request.password);
 
     const newEmployee = await prisma.employee.create({
       data: {
-         ...request ,
+        company_branch_id: request.company_branch_id,
+        job_position_id: request.job_position_id,
+        employment_status_id: request.employment_status_id,
+        unique_id: request.unique_id,
+        first_name: request.first_name,
+        last_name: request.last_name,
+        email: request.email,
+        password: hashedPassword,
+        phone_number: request.phone_number,
+        place_of_birth: request.place_of_birth,
+        birth_date: request.birth_date,
+        marital_status: request.marital_status,
+        blood_type: request.blood_type,
+        religion: request.religion,
+        identity_type: request.identity_type,
+        identity_number: request.identity_number,
+        identity_expired_date: request.identity_expired_date,
+        postcal_code: request.postcal_code,
+        citizen_id_address: request.citizen_id_address,
+        residential_address: request.residential_address,
+        bank_account_number: request.bank_account_number,
+        bank_type: request.bank_type,
+        wage: request.wage,
       },
     });
+
+    // const newEmployee = await prisma.employee.create({
+    //   data: {
+    //      ...request ,
+    //   },
+    // });
 
     return {
       employee_id: request.unique_id,
@@ -112,7 +150,7 @@ export class AccountService {
         company_branch_id: request.company_branch_id,
         employee_id: request.employee_id,
       },
-      data: {...request},
+      data: { ...request },
     });
 
     return employeeUpdate;
