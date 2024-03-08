c0f1a34941fcbaa82e4536956c091131cae78523
dimpram101 <dimas2004211@gmail.com>
2024-03-09 00:35:37 +0800

    fix: fix retrieve token

--- src/middlewares/jwt_middleware.ts
+++ src/middlewares/jwt_middleware.ts
@@ -4,16 +4,16 @@ import { Request, Response, NextFunction } from "express";
 
 export class JWTMiddleware {
   static async verifyToken(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      throw new ErrorResponse("Unauthorized", 401, ["token"], "UNAUTHORIZED");
-    }
     try {
+      const token = req.headers.authorization?.split(" ")[1];
+      if (!token) {
+        throw new ErrorResponse("Unauthorized", 401, ["token"], "UNAUTHORIZED");
+      }
       const decoded = jwt.verify(token, process.env.JWT_SECRET!);
       res.locals.user = decoded;
       next();
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 }
