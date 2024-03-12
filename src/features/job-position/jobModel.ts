d005b93a2f62e6712a983b4b9f3860c916085b22
Rothiii <drome.emord@gmail.com>
2024-03-12 18:20:23 +0800

    feat: Finish develop job position api

--- src/features/job-position/jobModel.ts
+++ src/features/job-position/jobModel.ts
@@ -1,10 +1,17 @@
+import { JobPosition } from '@prisma/client'
+
 export type CreateJobPositionRequest = {
   company_branch_id: number;
   name: string;
 };
 
 export type CreateJobPositionResponse = {
-  job_position_id: number;
   company_branch_id: number;
   name: string;
 }
+
+export type GetJobPositionRequest = {
+  company_branch_id: number;
+}
+
+export type GetJobPositionResponse = JobPosition[]
