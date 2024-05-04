584cd5f8e5b3e21958839f21d3a95a5e6a55d1d5
Rothiii <drome.emord@gmail.com>
2024-05-05 00:14:30 +0800

    feat: delete job position API

--- src/features/job-position/jobRoute.ts
+++ src/features/job-position/jobRoute.ts
@@ -6,5 +6,6 @@ const jobPositionRoute: Router = Router();
 jobPositionRoute.get('/:company_branch_id', JobPositionController.getJobPosition);
 jobPositionRoute.post('/', JobPositionController.createJobPosition);
 jobPositionRoute.put('/update', JobPositionController.updateJobPosition);
+jobPositionRoute.delete('/delete', JobPositionController.deleteJobPosition);
 
 export default jobPositionRoute;
