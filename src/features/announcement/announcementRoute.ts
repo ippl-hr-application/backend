dced672a0d4b48451d801523aeb720e471cce772
Rothiii <drome.emord@gmail.com>
2024-05-07 20:41:38 +0800

    fix: add verify token

--- src/features/announcement/announcementRoute.ts
+++ src/features/announcement/announcementRoute.ts
@@ -7,22 +7,28 @@ const announcementRoute: Router = Router();
 
 announcementRoute.post('/', [
   upload.single('announcement_file'),
+  JWTMiddleware.verifyToken,
   AnnouncementController.addAnnouncement,
 ]);
 
 announcementRoute.get('/:company_id', [
+  JWTMiddleware.verifyToken,
   AnnouncementController.getAnnouncementCompany,
 ]);
 
-announcementRoute.get('/:company_id/title', [
+announcementRoute.get('/:company_id/:title', [
+  JWTMiddleware.verifyToken,
   AnnouncementController.getAnnouncementByTitle,
 ]);
 
 announcementRoute.get('/download/:company_id/:company_announcement_id', [
+  JWTMiddleware.verifyToken,
   AnnouncementController.downloadAnnouncementFile,
-  ]);
+]);
 
-announcementRoute.delete('/company/:company_id/announcement/:announcement_id', AnnouncementController.deleteAnnouncement)
-
+announcementRoute.delete('/company/:company_id/announcement/:announcement_id', [
+  JWTMiddleware.verifyToken,
+  AnnouncementController.deleteAnnouncement,
+]);
 
 export default announcementRoute;
