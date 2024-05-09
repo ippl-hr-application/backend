84dac827f79edb8d130a4b37e67b42183aaa182b
dimpram101 <dimas2004211@gmail.com>
2024-05-09 22:24:21 +0800

    feat: add new migration

---
+++ prisma/migrations/20240509142409_employees_owner_and_reset_password_tokens/migration.sql
@@ -0,0 +1,57 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `shift_id` on the `attendances` table. All the data in the column will be lost.
+  - Added the required column `assign_shift_id` to the `attendances` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `date` to the `company_announcements` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "attendances" DROP CONSTRAINT "attendances_shift_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "company_file_templates" DROP CONSTRAINT "company_file_templates_company_id_fkey";
+
+-- DropIndex
+DROP INDEX "attendance_checks_attendance_id_key";
+
+-- DropIndex
+DROP INDEX "employees_email_key";
+
+-- AlterTable
+ALTER TABLE "attendances" DROP COLUMN "shift_id",
+ADD COLUMN     "assign_shift_id" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "companies" ALTER COLUMN "package_type" SET DEFAULT 'FREE';
+
+-- AlterTable
+ALTER TABLE "company_announcements" ADD COLUMN     "date" DATE NOT NULL;
+
+-- CreateTable
+CREATE TABLE "attendance_submission" (
+    "attendance_submission_id" SERIAL NOT NULL,
+    "submision_id" INTEGER NOT NULL,
+    "attendance_id" INTEGER NOT NULL,
+    "reason" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "attendance_submission_pkey" PRIMARY KEY ("attendance_submission_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "attendance_submission_submision_id_key" ON "attendance_submission"("submision_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "attendance_submission_attendance_id_key" ON "attendance_submission"("attendance_id");
+
+-- AddForeignKey
+ALTER TABLE "company_file_templates" ADD CONSTRAINT "company_file_templates_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "company_branches"("company_branch_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "attendances" ADD CONSTRAINT "attendances_assign_shift_id_fkey" FOREIGN KEY ("assign_shift_id") REFERENCES "assign_shifts"("assign_shift_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "attendance_submission" ADD CONSTRAINT "attendance_submission_attendance_id_fkey" FOREIGN KEY ("attendance_id") REFERENCES "attendances"("attendance_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "attendance_submission" ADD CONSTRAINT "attendance_submission_submision_id_fkey" FOREIGN KEY ("submision_id") REFERENCES "submissions"("submission_id") ON DELETE CASCADE ON UPDATE CASCADE;
