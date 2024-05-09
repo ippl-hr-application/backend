9cc14fb8169acb75a333c682b4886b19bcf32f1f
dimpram101 <dimas2004211@gmail.com>
2024-05-09 21:48:48 +0800

    fix: add checking company branch id middleware

--- src/features/document-template-management/template-route.ts
+++ src/features/document-template-management/template-route.ts
@@ -2,28 +2,33 @@ import { Router } from "express";
 import { JWTMiddleware } from "../../middlewares/jwt_middleware";
 import { TemplateController } from "./template-controller";
 import { upload } from "../../middlewares/multer";
+import { CompanyMiddleware } from "../../middlewares/company_middleware";
 
 const templateRoute: Router = Router();
 
 templateRoute.get("/:company_branch_id", [
   JWTMiddleware.verifyToken,
+  CompanyMiddleware.isCompanyBranchBelongsToCompany,
   TemplateController.getAllTemplateDocuments,
 ])
 
 templateRoute.post("/:company_branch_id", [
   JWTMiddleware.verifyToken,
+  CompanyMiddleware.isCompanyBranchBelongsToCompany,
   upload.single("template_file"),
   TemplateController.addNewTemplateDocument,
 ])
 
 templateRoute.post("/:company_branch_id/:template_id", [
   JWTMiddleware.verifyToken,
+  CompanyMiddleware.isCompanyBranchBelongsToCompany,
   upload.single("template_file"),
   TemplateController.updateTemplateDocument,
 ])
 
 templateRoute.delete("/:company_branch_id/:template_id", [
   JWTMiddleware.verifyToken,
+  CompanyMiddleware.isCompanyBranchBelongsToCompany,
   TemplateController.deleteTemplateDocument,
 ])
 
