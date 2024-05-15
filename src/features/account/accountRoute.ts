82287b75ef9a40c3644a2e00f44211763ba45656
Rothiii <drome.emord@gmail.com>
2024-05-15 16:47:29 +0800

    feat: update soft delete & add new API permanent delete, create service, getAll service
    soft delete for account is make hasResigned true and add a Date to delete_at, delete permanent is remove from db, create account service can make an account when there are same email in the same company branch with account has been deleted, and the last is get all employee adding filter job position name, employment status, and deleted If deleted equals to true, return deleted account only.

--- src/features/account/accountRoute.ts
+++ src/features/account/accountRoute.ts
@@ -43,12 +43,16 @@ accountRoute.patch('/:company_branch_id/resigned', [
   JWTMiddleware.verifyToken,
   AccountController.employeeResign,
 ]);
-// accountRoute.patch('/branch/:company_branch_id/employee/:employee_id', AccountController.softDeleteEmployee);
 
-// Delete an employee
-accountRoute.delete('/branch/:company_branch_id/employee/:employee_id', [
+// soft Delete an employee, set deleted_at to current date and still keep the data
+accountRoute.delete('/branch/:company_branch_id/employee/:employee_id/softDelete', [
   JWTMiddleware.verifyToken,
-  AccountController.deleteEmployee,
+  AccountController.softDeleteEmployee,
 ]);
 
+// Literally Delete from database
+accountRoute.delete('/branch/:company_branch_id/employee/:employee_id/deletePermanent', [
+  JWTMiddleware.verifyToken,
+  AccountController.deleteEmployeeFromDatabase,
+]);
 export default accountRoute;
