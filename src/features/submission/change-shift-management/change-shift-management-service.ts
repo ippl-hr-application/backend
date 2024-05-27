8c8ea1156b6911f2a8a1e9ab951e744348ae9623
Muhammad Saman <samanmuhammad077@gmail.com>
2024-05-28 03:33:21 +0800

    fix: add notif email when validate submission

--- src/features/submission/change-shift-management/change-shift-management-service.ts
+++ src/features/submission/change-shift-management/change-shift-management-service.ts
@@ -10,6 +10,7 @@ import { ChangeShiftManagementValidation } from "./change-shift-management-valid
 import { ShiftService } from "../../shift/shift-service";
 import { deleteFile } from "../../../utils/delete_file";
 import { start } from "repl";
+import { sendEmail } from "../../../utils/nodemailer";
 
 export class ChangeShiftManagementService {
   static async getAllByCompanyBranchId(
@@ -147,6 +148,9 @@ export class ChangeShiftManagementService {
           select: {
             company_branch_id: true,
             employee_id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
           },
         },
         change_shift_submission: {
@@ -176,6 +180,80 @@ export class ChangeShiftManagementService {
         });
       }
     });
+    const htmlTemplate = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  line-height: 1.6;
+                  color: #333;
+                  background-color: #f4f4f4;
+                  margin: 0;
+                  padding: 0;
+              }
+              .container {
+                  width: 80%;
+                  max-width: 600px;
+                  margin: 0 auto;
+                  background-color: #ffffff;
+                  padding: 20px;
+                  border-radius: 10px;
+                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+              }
+              h1 {
+                  color: #444444;
+              }
+              p {
+                  font-size: 1rem;
+              }
+              .strike-through {
+                  text-decoration: line-through;
+              }
+              .credentials {
+                  background-color: #f9f9f9;
+                  padding: 15px;
+                  border: 1px solid #ddd;
+                  border-radius: 5px;
+                  margin-top: 20px;
+              }
+              .credentials p {
+                  margin: 5px 0;
+              }
+              .footer {
+                  margin-top: 30px;
+                  font-size: 0.8rem;
+                  color: #777777;
+              }
+          </style>
+      </head>
+      <body>
+          <div class="container">
+              <h1>Hallo, ${letter?.employee?.first_name} ${
+      letter?.employee?.last_name
+    }!</h1>
+              <p>Pengajuan ganti shift yang anda lakukan telah ${
+                status == "ACCEPTED" ? "diterima" : "ditolak"
+              } oleh Perusahaan. Mohon segera membuka aplikasi untuk informasi lebih lanjut</p>
+              <p>Harap simpan informasi ini dengan aman dan jangan bagikan dengan siapa pun.</p>
+              
+              <div class="footer">
+                  <p>&copy; ${new Date().getFullYear()} Perusahaan Kami. Semua hak dilindungi.</p>
+              </div>
+          </div>
+      </body>
+      </html>
+      `;
+    const mailOptions = {
+      from: `Meraih <${process.env.EMAIL}>`, // sender address
+      to: letter?.employee?.email, // list of receivers
+      subject: "Change Shift Approval", // Subject line
+      html: htmlTemplate, // html body
+    };
+    sendEmail(mailOptions);
   }
   static async deleteLetter(submission_id: number, company_branch_id: string) {
     const request = Validation.validate(
--- src/features/submission/sick-management/sick-management-service.ts
+++ src/features/submission/sick-management/sick-management-service.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../../applications";
 import { ErrorResponse } from "../../../models";
 import { deleteFile } from "../../../utils/delete_file";
+import { sendEmail } from "../../../utils/nodemailer";
 import { Validation } from "../../../validations";
 import {
   GetAllByCompanyBranchIdResponse,
@@ -124,6 +125,15 @@ export class SickManagementService {
           company_branch_id: request.company_branch_id,
         },
       },
+      select: {
+        employee: {
+          select: {
+            first_name: true,
+            last_name: true,
+            email: true,
+          },
+        },
+      },
     });
     if (!letter) {
       throw new ErrorResponse(`Sick not found`, 404, ["submission_id"]);
@@ -136,6 +146,80 @@ export class SickManagementService {
         status: request.status,
       },
     });
+    const htmlTemplate = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                line-height: 1.6;
+                color: #333;
+                background-color: #f4f4f4;
+                margin: 0;
+                padding: 0;
+            }
+            .container {
+                width: 80%;
+                max-width: 600px;
+                margin: 0 auto;
+                background-color: #ffffff;
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+            h1 {
+                color: #444444;
+            }
+            p {
+                font-size: 1rem;
+            }
+            .strike-through {
+                text-decoration: line-through;
+            }
+            .credentials {
+                background-color: #f9f9f9;
+                padding: 15px;
+                border: 1px solid #ddd;
+                border-radius: 5px;
+                margin-top: 20px;
+            }
+            .credentials p {
+                margin: 5px 0;
+            }
+            .footer {
+                margin-top: 30px;
+                font-size: 0.8rem;
+                color: #777777;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Hallo, ${letter?.employee?.first_name} ${
+      letter?.employee?.last_name
+    }!</h1>
+            <p>Pengajuan sakit yang anda lakukan telah ${
+              status == "ACCEPTED" ? "diterima" : "ditolak"
+            } oleh Perusahaan. Mohon segera membuka aplikasi untuk informasi lebih lanjut</p>
+            <p>Harap simpan informasi ini dengan aman dan jangan bagikan dengan siapa pun.</p>
+            
+            <div class="footer">
+                <p>&copy; ${new Date().getFullYear()} Perusahaan Kami. Semua hak dilindungi.</p>
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+    const mailOptions = {
+      from: `Meraih <${process.env.EMAIL}>`, // sender address
+      to: letter?.employee?.email, // list of receivers
+      subject: "Sick Approval", // Subject line
+      html: htmlTemplate, // html body
+    };
+    sendEmail(mailOptions);
   }
   static async deleteLetter(submission_id: number, company_branch_id: string) {
     const request = Validation.validate(SickManagementValidation.GET_BY_ID, {
